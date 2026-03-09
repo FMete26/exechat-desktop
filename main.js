cf2dac692e1e2e03eff610729907716fcebe9536
Mahmut Enes Güner <mahmutenesguner@Mahmut-Mac-mini.local>
2026-03-09 06:16:01 +0300

    Fix WebRTC black screen

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const { app, BrowserWindow, shell, session } = require('electron');
 
 let mainWindow;
 
+app.commandLine.appendSwitch('enable-usermedia-screen-capturing');
+app.commandLine.appendSwitch('allow-http-screen-capture');
+app.commandLine.appendSwitch('use-fake-ui-for-media-stream');
+app.commandLine.appendSwitch('enable-experimental-web-platform-features');
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
@@ -13,15 +18,16 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      webSecurity: false
+      webSecurity: false,
+      allowRunningInsecureContent: false,
+      experimentalFeatures: true
     },
     autoHideMenuBar: true,
     show: false
   });
 
   session.defaultSession.setPermissionRequestHandler((webContents, permission, callback) => {
-    const allowed = ['media', 'microphone', 'camera', 'screen', 'audioCapture', 'videoCapture', 'desktopCapture'];
-    callback(allowed.includes(permission));
+    callback(true);
   });
 
   session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
@@ -29,8 +35,12 @@ function createWindow() {
   });
 
   mainWindow.loadURL('https://exechat.duckdns.org');
-  
   mainWindow.once('ready-to-show', () => mainWindow.show());
+
+  mainWindow.webContents.on('render-process-gone', (event, details) => {
+    console.log('Render process gone:', details.reason);
+    createWindow();
+  });
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     shell.openExternal(url);
